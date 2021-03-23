b5a9d81cdc45677904d2415411ea0d78d453469e
Leon Bungert <leon.bungert@fau.de>
2021-03-23 11:59:03 +0100

    added comparison of proximal power methods and nossek flow

---
+++ code_proximals/RUNME_proximal_eigenvectors.py
@@ -0,0 +1,200 @@
+# standard
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+import matplotlib.ticker as mtick
+import os
+# ODL
+import odl 
+# custom
+from algorithms import proximal_power_method, nossek_flow
+
+# set up image domain
+X = odl.uniform_discr([-1, -1], [1, 1], [100, 100], interp='linear')
+
+# choose model and algorithm parameters
+iso = False             # isotropic [True] or anisotropic [False] inner gradient norm
+discr = 'forward'       # discretization of the gradient: 'forward', 'backward', 'central'
+p_grad = 1              # outer gradient norm: total_variation: [p_grad=1] and H1 [p_grad=2]
+sig = 0.05*1            # noise added to initial condition
+save2disk = True        # save images to disk {True, False}
+init = 'disk_ellipse'   # initial condition {'disk','square','noise','ellipse'}
+i_max = 30              # maximum number of successful power iterations
+
+#%% set up initial condition
+if init == 'disk':
+    data_func = lambda x: x[0]**2 + x[1]**2 < 0.7**2    
+    data = X.element(data_func)
+if init == 'disk_ellipse':
+    data_func = lambda x: x[0]**2 + x[1]**2 < 0.7**2    
+    data = X.element(data_func)
+    data -= odl.phantom.geometric.defrise(X, nellipses=1)      
+elif init == 'square': 
+    data_func = odl.phantom.cuboid(X)
+    data = X.element(data_func)
+elif init == 'noise':
+    data_func = odl.phantom.white_noise(X)
+    data = X.element(data_func)
+elif init == 'ellipse':
+    data = odl.phantom.cuboid(X) - odl.phantom.geometric.defrise(X, nellipses=1)      
+else:
+    raise ValueError('Unknown initialization!')
+
+# add noise
+noise = odl.phantom.white_noise(X, mean=0, stddev = sig)
+data += noise
+
+# normalize data
+H = odl.solvers.L2Norm(X)
+proj = np.mean
+data -= proj(data)
+data /= H(data)
+    
+
+#%% set up functionals and operators
+grad = odl.Gradient(X, method=discr, pad_mode='symmetric')
+L = grad    
+if discr == 'central':
+    opNorm = np.sqrt(sum(1/X.cell_sides**2))
+else:
+    opNorm = 2*np.sqrt(sum(1/X.cell_sides**2))
+
+L.norm = opNorm
+
+if p_grad == 1:
+    if iso is True:
+        gradnorm = odl.solvers.GroupL1Norm(L.range, 2)
+    else:
+        gradnorm = odl.solvers.GroupL1Norm(L.range, 1)
+elif p_grad == 2:    
+    gradnorm = odl.solvers.L2Norm(L.range)
+
+# define regularizer
+g = gradnorm
+J = g * L
+
+#%% create output folder
+if save2disk:
+    folder = 'results/' + init
+    if not os.path.exists(folder):
+                os.makedirs(folder)
+else:
+    folder = None                    
+
+
+affinities_cont = []
+angles_cont = []
+
+#%% run nossek flow
+rules_nossek = ['Nossek']
+if save2disk:
+    folder_nossek = folder + '/nossek'
+    if not os.path.exists(folder_nossek):
+                os.makedirs(folder_nossek)
+else:
+    folder_nossek = None 
+    
+u, affs, angles = nossek_flow(data, J, H, folder=folder_nossek, proj=proj,\
+                              save2disk=save2disk, i_max=i_max)
+    
+affinities_cont.append(affs)
+angles_cont.append(angles)
+
+#%% run power method
+rules_power = ['Feld 0.01', 'Feld 0.1', 'Feld 1', 'Constant', 'Variable']
+
+for rule in rules_power:
+    if save2disk:
+        folder_power = folder + '/power/' + rule
+        if not os.path.exists(folder_power):
+            os.makedirs(folder_power)
+    else:
+        folder_power = None 
+        
+    u, affs, angles = proximal_power_method(data, J, H, folder=folder_power,\
+                                            proj=proj,save2disk=save2disk,\
+                                            i_max=i_max, rule=rule)
+    affinities_cont.append(affs)
+    angles_cont.append(angles)
+
+
+#%% Output settings
+# use LaTeX fonts in the plot
+plt.rc('text', usetex=True)
+
+font = {'family' : 'serif',
+        'size'   : 15}
+
+matplotlib.rc('font', **font)
+
+
+#%% Output only affinities
+rules = rules_nossek + rules_power
+
+markers = [None,None, None, None, None, None]
+linestyles = ['-',\
+              '-.',\
+              '--',\
+              '-',\
+              '--',\
+              '-']
+    
+colors = ['black', 'blue', 'blue', 'blue', 'red', 'red']
+    
+linewidth = 1.5
+
+f, ax = plt.subplots()
+ax.set_xlabel('Iterations')
+
+ax.yaxis.set_major_formatter(mtick.PercentFormatter(xmax=1.0,decimals=0))
+ax.set_xticks(range(0,i_max,5))
+
+ax.set_ylabel('Affinity')  
+for i in range(len(affinities_cont)):        
+    ax.plot(affinities_cont[i], label=rules[i],color=colors[i],\
+              marker=markers[i],\
+              linestyle=linestyles[i],\
+              linewidth=linewidth)
+ax.tick_params(axis='y')
+ax.legend(loc='lower right')
+plt.tight_layout()      
+     
+if save2disk:   
+    plt.savefig(folder + '/affinities.png',dpi=1000)
+
+#%% Output only angles
+rules = rules_nossek + rules_power
+
+markers = [None,None, None, None, None, None]
+linestyles = ['-',\
+              '-.',\
+              '--',\
+              '-',\
+              '--',\
+              '-']
+    
+colors = ['black', 'blue', 'blue', 'blue', 'red', 'red']
+    
+linewidth = 1.5
+
+f, ax = plt.subplots()
+ax.set_xlabel('Iterations')
+
+ax.set_xticks(range(0,i_max,5))
+
+ax.set_ylabel('Angle')  
+for i in range(len(angles_cont)):    
+    angles_plot = [360/(2*np.pi) * np.arccos(1 - phi) for phi in angles_cont[i]]   
+    ax.semilogy(angles_plot, label=rules[i],color=colors[i],\
+              marker=markers[i],\
+              linestyle=linestyles[i],\
+              linewidth=linewidth)
+ax.tick_params(axis='y')
+ax.legend(loc='upper right')
+plt.tight_layout()      
+     
+if save2disk:   
+    plt.savefig(folder + '/angles.png',dpi=1000)    
+
+        
+        
---
+++ code_proximals/algorithms.py
@@ -0,0 +1,271 @@
+import numpy as np
+import odl
+import matplotlib.pyplot as plt
+
+##############################################################################
+def proximal_power_method(data, J, H, folder=None, proj=None, save2disk=False,\
+                          i_max=10, rule='Constant'):
+    
+    X = data.space
+    g = J.left
+    L = J.right
+    
+    if proj is None:
+        proj = odl.ZeroOperator(X)
+    
+    # get primal dual step sizes
+    (tau, sigma) = odl.solvers.pdhg_stepsize(L.norm)
+    
+    #%% intialize
+    
+    # initialize iterates and eigenvalues
+    ev_m = np.inf
+    data -= proj(data)
+    data /= H(data)
+    u = data.copy()
+    um = u.copy()
+    
+    # choose step size constant 0 < c < 1
+    c = 0.9
+    
+    # plot initialization
+    u.show('Iteration ' + str(0))
+    
+    
+    #
+    rules = rule.split(' ')
+    
+    # define step size
+    if rules[0] == 'Constant':
+        alpha = c * H(um-proj(um))/J(um)
+    
+    # initialize iteration counter and output vectors
+    i = 0
+    affs = []
+    angles = []
+    
+    # output 
+    if save2disk and folder is not None:        
+        filename = '_it_' + str(i)
+        plt.imsave(folder + '/' + filename + '.png', u, cmap='gray')
+    
+    #%% run power method
+    it = 0  # couter for proximal backtracking
+    it_max = 10 # maximum number of prox evaluations
+    while i < i_max:
+    
+        # define variable step size
+        if rules[0] == 'Variable':
+            alpha = c /J(um)
+        elif rules[0] == 'Feld':
+            if len(rules) > 1:
+                dt = float(rules[1])
+            else:
+                dt = 1.
+            alpha = c * dt / (1 + dt * J(um)) 
+            
+        # define data fidelity    
+        f = (0.5/alpha) * odl.solvers.L2NormSquared(X).translated(um)
+        
+        # evaluate the proximal operator    
+        odl.solvers.pdhg(u,f,g,L,1000,tau,sigma) 
+        
+        # compute subgradient
+        sg = (um - u)/alpha    
+       
+        # subtract mean to be on the safe side
+        u -= proj(u)
+        
+        # evaluate proximal eigenvalue
+        ev_prox = u.inner(um)/(H(um)**2)
+          
+        # normalize to create the iterate
+        u /= H(u)
+        
+        # evaluate Rayleigh quotient
+        ev = J(u)/H(u-proj(u))
+    
+        # check whether Rayleigh quotient decreased
+        if ev < ev_m:
+            it = 0
+                        
+            # compute eigenvector affinity in <= 1
+            aff = odl.solvers.L2NormSquared(X)(sg)/J(sg)
+            affs.append(aff)
+            
+            # evaluate angle            
+            angle = 1 - um.inner(u)/(H(u)*H(um))
+            angles.append(angle)
+                
+            # output
+            print(20*'<>')                      
+            print('Affinity: {:.2f}'.format(aff))
+            print('Angle: {:.2E} '.format(angle))
+            print('Subdiff Eigenvalue: {:.2f} '.format(ev))
+            print('Prox Eigenvalue: {:.2f} '.format(ev_prox))
+            print('Theoretical Prox Eigenvalue: {:.2f} '.format(1-alpha*ev))
+            print(20*'<>')                      
+        
+            # update iteration counter and plot image
+            i += 1
+            u.show('Iteration ' + str(i))
+            
+            # save result to disk
+            if save2disk and folder is not None:        
+                filename = 'it_' + str(i)
+                plt.imsave(folder + '/' + filename + '.png', u, cmap='gray')
+                
+            # update old variables    
+            um = u.copy()
+            ev_m = ev
+        else:
+            it += 1
+            c *= 0.9
+            
+            if it >= it_max: 
+                
+                #evaluate angle                
+                tmp = um.copy()
+                f = (0.5/alpha) * odl.solvers.L2NormSquared(X).translated(tmp)        
+                odl.solvers.pdhg(um,f,g,L,1000,tau,sigma) 
+                angle = 1-um.inner(tmp)/(H(um)*H(tmp))
+                angles.append(angle)
+                
+                #evaluate affinity
+                sg = (um-tmp)/alpha
+                aff = odl.solvers.L2NormSquared(X)(sg)/J(sg)
+                affs.append(aff)
+                                                                           
+                print('Stopped iterations because Rayleigh quotient cannot be improved')
+                break
+            else:
+                continue
+            
+    return um, affs, angles     
+##############################################################################        
+def nossek_flow(data, J, H, folder=None, proj=None, \
+                save2disk=False, i_max=10):
+    
+    X = data.space
+    g = J.left
+    L = J.right
+    
+    if proj is None:
+        proj = odl.ZeroOperator(X)
+    
+    # get primal dual step sizes
+    (tau, sigma) = odl.solvers.pdhg_stepsize(L.norm)
+    
+    #%% intialize
+    
+    # initialize iterates and eigenvalues
+    ev_m = np.inf
+    data -= proj(data)
+    data /= H(data)
+    u = data.copy()
+    um = u.copy()
+    
+    # choose step size constant 0 < c < 1
+    c = 0.9
+    
+    # plot initialization
+    u.show('Iteration ' + str(0))
+ 
+    dt = c * H(um)
+    
+    # initialize iteration counter and output vectors
+    i = 0
+    affs = []
+    angles = []
+    
+    # output 
+    if save2disk and folder is not None:        
+        filename = '_it_' + str(i)
+        plt.imsave(folder + '/' + filename + '.png', u, cmap='gray')
+    
+    #%% run flow
+    it = 0  # couter for proximal backtracking
+    it_max = 10 # maximum number of prox evaluations
+    while i < i_max:            
+        
+        # define proximal parameter
+        if i == 0:
+            sg = L.adjoint(L(um))
+                    
+        norm_sg = H(sg)
+        alpha = dt / norm_sg         
+       
+        # define data fidelity    
+        f = (0.5/alpha) * odl.solvers.L2NormSquared(X).translated(um)
+        
+        # evaluate the proximal operator    
+        odl.solvers.pdhg(u,f,g,L,1000,tau,sigma) 
+        
+        # compute subgradient
+        sg = (um - u)/alpha                         
+        
+        # subtract mean to be on the safe side
+        u -= proj(u)
+
+        # normalize to create the iterate
+        u /= 1-dt/H(um)     
+        
+        # evaluate Rayleigh quotient
+        ev = J(u)/H(u-proj(u))
+    
+        # check whether Rayleigh quotient decreased
+        if ev < ev_m:
+            it = 0            
+            
+            # compute eigenvector affinity in <= 1
+            aff = odl.solvers.L2NormSquared(X)(sg)/J(sg)
+            affs.append(aff)
+            
+            # evaluate angle
+            # angle = 1 - um.inner(u)/(H(u)*H(um))
+            angle = 1 - sg.inner(u)/(H(sg)*H(u))
+            angles.append(angle)
+                
+            # output
+            print(20*'<>')                      
+            print('Affinity: {:.2f}'.format(aff))
+            print('Angle: {:.2E} '.format(angle))
+            print('Subdiff Eigenvalue: {:.2f} '.format(ev))
+            print(20*'<>')                      
+        
+            # update iteration counter and plot image
+            i += 1
+            u.show('Iteration ' + str(i))
+            
+            # save result to disk
+            if save2disk and folder is not None:        
+                filename = 'it_' + str(i)
+                plt.imsave(folder + '/' + filename + '.png', u, cmap='gray')
+                
+            # update old variables    
+            um = u.copy()
+            ev_m = ev
+        else:
+            it += 1
+            
+            if it >= it_max:
+                
+                #evaluate angle                
+                tmp = um.copy()
+                f = (0.5/alpha) * odl.solvers.L2NormSquared(X).translated(tmp)        
+                odl.solvers.pdhg(um,f,g,L,1000,tau,sigma) 
+                angle = 1-um.inner(tmp)/(H(um)*H(tmp))
+                angles.append(angle)
+                
+                #evaluate affinity
+                sg = (um-tmp)/alpha
+                aff = odl.solvers.L2NormSquared(X)(sg)/J(sg)
+                affs.append(aff)
+                
+                print('Stopped iterations because Rayleigh quotient cannot be improved')
+                break
+            else:
+                continue        
+     
+    return um, affs, angles     
+##############################################################################        

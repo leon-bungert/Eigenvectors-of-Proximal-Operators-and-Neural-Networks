ce7efb61fd56bf8636d9c81caa2c11a77d5a893c
Leon Bungert <leon.bungert@fau.de>
2021-03-23 12:05:03 +0100

    Added power method for FFDnet

---
+++ code_FFDnet/FFDnet_code/README.md
@@ -0,0 +1,11 @@
+# Network eigenfunctions
+
+Matlab code applying power iterations (and some related schemes) to the FFDnet network
+
+Requires:
+- FFDNET: https://github.com/cszn/FFDNet
+- MatConvnet: http://www.vlfeat.org/matconvnet/    
+
+# Use  
+test_ef
+
---
+++ code_FFDnet/RUNME_FFDnet_eigenvectors.m
@@ -0,0 +1,98 @@
+addpath(genpath('FFDnet_code'));
+clear all;
+close all;
+
+%% load net
+global sigmas; 
+sigmas = 15/255.;  %noise level for FFDnet
+load('FFDNet_gray.mat');
+net = vl_simplenn_tidy(net);
+
+%% load cameraman image
+f = double(imread('cameraman.tif'))/255;
+f = im2single(f);
+
+%% set parameters for power method
+it_num = 2000;    % number of iterations
+freq = 1;      % output frequency
+
+%% initalize power method
+[M,N] = size(f);
+u = f;
+
+% save original mean and norm
+orig_mean = mean(u(:));
+u_removed = u - orig_mean;
+orig_norm = norm(double(u_removed(:)));
+
+% define containers
+u_arr = zeros(M,N,it_num);
+Rayleigh_arr = zeros(1,it_num);
+theta_norm_arr = zeros(1,it_num);
+
+%% run power method
+figure;
+imshow(u,[])
+title('Initial condition');
+drawnow;
+
+tic;
+for i = 1 : it_num
+    res = vl_ffdnet_matlab(net,u);
+    u_T = res(end).x;
+         
+    m_u = mean(u(:));
+    
+    % remove mean from u
+    u = u - m_u;
+    
+    % remove mean from u_T
+    u_T = u_T - mean(u_T(:));
+      
+    % compute inner products and norms
+    u_u_T = sum(u(:).*u_T(:));
+    norm_u = norm(u(:));
+    norm_u_T = norm(u_T(:));
+    
+    % compute Rayleigh quotient
+    Rayleigh_arr(i) = u_u_T / (norm_u^2);
+    
+    % compute angle in degrees
+    theta_norm_arr(i)=acos(u_u_T / (norm_u * norm_u_T)) * 180/pi;
+
+    % normalize by original norm   
+    u_T = u_T / norm_u_T * orig_norm;
+    
+    % return to mean of u
+    u_T = u_T + m_u;
+      
+    % update variables
+    u = u_T;
+    u_arr(:,:,i) = u;
+    
+    % output
+    if mod(i, freq) == 0
+        fprintf(['Finished ', num2str(i), ' iterations of the power method\n'])
+        
+        imshow(u,[]);
+        title(['Iteration ', num2str(i)]);
+        drawnow;        
+    end
+end
+toc;
+%% plot validation measures
+figure;
+plot(Rayleigh_arr,'k','LineWidth',3)
+xlabel('\bf iterations','FontSize',40,'Interpreter','latex');
+ax = gca;
+ax.LineWidth = 1;
+set(gca, 'FontSize', 30)
+title('Rayleigh quotients');
+
+figure;
+plot(1:it_num, theta_norm_arr,'k','LineWidth',3)
+xlabel('\bf iterations','FontSize',40,'Interpreter','latex');
+ax = gca;
+ax.LineWidth = 1;
+set(gca, 'FontSize', 30)
+title('Angles');
